f1ba4b89b230166a31ec0e6a9842336a05b994ff
Antler9000 <ghenakfn20@naver.com>
2025-12-09 16:12:08 +0900

    [splay_tree] (병합 대기 필요) 마이크로소프트 D3D12 예제에 사용된 표기법으로 통일
    (병합 대기 사유)
    본 커밋이 반영한 BST_while의 표기법 수정은 아직 BST_while을 참조하는 AVL_tree, red_black_tree 단원들에 반영되지 않았기 때문이다. 이 두 단원들도 수정을 반영하면 그 때 main에 병합할 계획이다.

--- 4.스플레이트리/main_splay_tree.cpp
+++ 4.스플레이트리/main_splay_tree.cpp
@@ -2,64 +2,64 @@
 
 int main() {
 	SplayTree& test_tree = *(new SplayTree());
-	test_tree.insert(5, 1515);
-	test_tree.insert(7, 2727);
-	test_tree.insert(3, 1313);
-	test_tree.insert(4, 2424);
-	test_tree.insert(6, 3636);
-	test_tree.insert(1, 5151);
-	test_tree.insert(8, 5858);
-	test_tree.preorder_print();
+	test_tree.Insert(5, 1515);
+	test_tree.Insert(7, 2727);
+	test_tree.Insert(3, 1313);
+	test_tree.Insert(4, 2424);
+	test_tree.Insert(6, 3636);
+	test_tree.Insert(1, 5151);
+	test_tree.Insert(8, 5858);
+	test_tree.PreorderPrint();
 
-	cout << "retrieve test 8" << endl;
-	test_tree.retrieve(8);
-	test_tree.preorder_print();
+	cout << "Retrieve test 8" << endl;
+	test_tree.Retrieve(8);
+	test_tree.PreorderPrint();
 
-	cout << "retrieve test 4" << endl;
-	test_tree.retrieve(4);
-	test_tree.preorder_print();
+	cout << "Retrieve test 4" << endl;
+	test_tree.Retrieve(4);
+	test_tree.PreorderPrint();
 
-	cout << "retrieve test 3" << endl;
-	test_tree.retrieve(3);
-	test_tree.preorder_print();
+	cout << "Retrieve test 3" << endl;
+	test_tree.Retrieve(3);
+	test_tree.PreorderPrint();
 
-	cout << "retrieve test 3" << endl;
-	test_tree.retrieve(3);
-	test_tree.preorder_print();
+	cout << "Retrieve test 3" << endl;
+	test_tree.Retrieve(3);
+	test_tree.PreorderPrint();
 
-	cout << "retrieve test 7" << endl;
-	test_tree.retrieve(7);
-	test_tree.preorder_print();
+	cout << "Retrieve test 7" << endl;
+	test_tree.Retrieve(7);
+	test_tree.PreorderPrint();
 
-	cout << "retrieve test 5" << endl;
-	test_tree.retrieve(5);
-	test_tree.preorder_print();
+	cout << "Retrieve test 5" << endl;
+	test_tree.Retrieve(5);
+	test_tree.PreorderPrint();
 
 
 	SplayTree& copied_test_tree = *(new SplayTree());
-	copied_test_tree.copy_from(&test_tree);
-	copied_test_tree.preorder_print();
+	copied_test_tree.CopyFrom(&test_tree);
+	copied_test_tree.PreorderPrint();
 
-	test_tree.remove(7);
-	test_tree.preorder_print();
+	test_tree.Remove(7);
+	test_tree.PreorderPrint();
 
-	test_tree.remove(3);
-	test_tree.preorder_print();
+	test_tree.Remove(3);
+	test_tree.PreorderPrint();
 
-	test_tree.remove(5);
-	test_tree.preorder_print();
+	test_tree.Remove(5);
+	test_tree.PreorderPrint();
 
-	test_tree.remove(4);
-	test_tree.preorder_print();
+	test_tree.Remove(4);
+	test_tree.PreorderPrint();
 
-	test_tree.remove(6);
-	test_tree.preorder_print();
+	test_tree.Remove(6);
+	test_tree.PreorderPrint();
 
-	test_tree.remove(1);
-	test_tree.preorder_print();
+	test_tree.Remove(1);
+	test_tree.PreorderPrint();
 
-	test_tree.remove(8);
-	test_tree.preorder_print();
+	test_tree.Remove(8);
+	test_tree.PreorderPrint();
 
 	delete& test_tree;
 	return 0;
